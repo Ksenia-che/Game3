dfa212c81e7d1f008175c97233036726978323a3
forgetwrw <bredok3211@gmail.com>
2026-04-07 18:06:29 +0300

    game

--- core/src/ru/samsung/gamestudio/MyGdxGame.java
+++ core/src/ru/samsung/gamestudio/MyGdxGame.java
@@ -1,31 +1,50 @@
 package ru.samsung.gamestudio;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Box2D;
+import com.badlogic.gdx.physics.box2d.World;
 import com.badlogic.gdx.utils.ScreenUtils;
+import screens.GameScreen;
 
-public class MyGdxGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
+import static ru.samsung.gamestudio.GameSettings.*;
+
+public class MyGdxGame extends Game {
+	public SpriteBatch batch;
+	public OrthographicCamera camera;
+	public GameScreen gameScreen;
+	public World world;
+	float accumulator = 0;
+
+	public void stepWorld() {
+		float delta = Gdx.graphics.getDeltaTime();
+		accumulator += delta;
+
+		if (accumulator >= STEP_TIME) {
+			accumulator -= STEP_TIME;
+			world.step(STEP_TIME, VELOCITY_ITERATIONS, POSITION_ITERATIONS);
+		}
+	}
 	@Override
-	public void create () {
+	public void create() {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		camera = new OrthographicCamera();
+		camera.setToOrtho(false, GameSettings.SCREEN_WIDTH, GameSettings.SCREEN_HEIGHT);
+
+		gameScreen = new GameScreen(this);
+		setScreen(gameScreen);
+		Box2D.init();
+		world = new World(new Vector2(0, 0), true);
+		world.step(STEP_TIME, VELOCITY_ITERATIONS, POSITION_ITERATIONS);
 	}
 
 	@Override
-	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
-	
-	@Override
-	public void dispose () {
+	public void dispose() {
 		batch.dispose();
-		img.dispose();
 	}
 }
---
+++ core/src/screens/GameScreen.java
@@ -0,0 +1,11 @@
+package screens;
+
+import ru.samsung.gamestudio.MyGdxGame;
+
+public class GameScreen extends ScreenAdapter {
+    MyGdxGame myGdxGame;
+
+    public GameScreen(MyGdxGame myGdxGame) {
+        this.myGdxGame = myGdxGame;
+    }
+}
