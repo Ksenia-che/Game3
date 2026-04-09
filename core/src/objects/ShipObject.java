5d925f6ce0637f1d868761daab5204118eba5ef6
forgetwrw <bredok3211@gmail.com>
2026-04-09 19:07:44 +0300

    game

---
+++ core/src/objects/ShipObject.java
@@ -0,0 +1,9 @@
+package objects;
+
+import com.badlogic.gdx.physics.box2d.World;
+
+public class ShipObject {
+    public ShipObject(int x, int y, int width, int height, String texturePath, World world) {
+        super();
+    }
+}
--- core/src/screens/GameScreen.java
+++ core/src/screens/GameScreen.java
@@ -1,11 +1,36 @@
 package screens;
 
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.utils.ScreenUtils;
+import objects.ShipObject;
+import ru.samsung.gamestudio.GameResources;
+import ru.samsung.gamestudio.GameSettings;
 import ru.samsung.gamestudio.MyGdxGame;
 
 public class GameScreen extends ScreenAdapter {
     MyGdxGame myGdxGame;
+    ShipObject shipObject;
 
     public GameScreen(MyGdxGame myGdxGame) {
         this.myGdxGame = myGdxGame;
+
+        shipObject = new ShipObject(
+                GameSettings.SCREEN_WIDTH / 2, 150,
+                GameSettings.SHIP_WIDTH, GameSettings.SHIP_HEIGHT,
+                GameResources.SHIP_IMG_PATH,
+                myGdxGame.world
+        );
+    }
+    @Override
+    public void render(float delta) {
+        myGdxGame.stepWorld();
+
+        myGdxGame.camera.update();
+        myGdxGame.batch.setProjectionMatrix(myGdxGame.camera.combined);
+        ScreenUtils.clear(Color.CLEAR);
+
+        myGdxGame.batch.begin();
+        shipObject.draw(myGdxGame.batch);
+        myGdxGame.batch.end();
     }
 }
