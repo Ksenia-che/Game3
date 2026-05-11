1065704f72a1d15d5ca242af7ffbe34d75e8174c
Ksenia-che <ksenia_chernova_8@mail.ru>
2026-05-11 17:51:49 +0300

    game

--- core/src/ru/samsung/gamestudio/GameResources.java
+++ core/src/ru/samsung/gamestudio/GameResources.java
@@ -1,10 +1,10 @@
 package ru.samsung.gamestudio;
 
 public class GameResources {
-    public static final String SHIP_IMG_PATH = "textures/ship.png";
-    public static final String TRASH_IMG_PATH = "textures/trash.png";
+    public static final String SHIP_IMG_PATH = "textures/ship.antivirus.png";
+    public static final String TRASH_IMG_PATH = "textures/trash.virus.png";
     public static final String BULLET_IMG_PATH = "textures/bullet.png";
-    public static final String BACKGROUND_IMG_PATH = "textures/background.png";
+    public static final String BACKGROUND_IMG_PATH = "textures/background.bluescreen.png";
     public static final String BLACKOUT_TOP_IMG_PATH = "textures/blackout_top.png";
     public static final  String LIVE_IMG_PATH = "textures/life.png";
     public static final  String FONT_PATH = "Montserrat-Bold.ttf";
